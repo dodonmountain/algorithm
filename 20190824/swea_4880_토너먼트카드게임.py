5e7efe5efee5463805dc5a348870fbbc71db1886
dodonmountain <lkh151515@gmail.com>
2019-08-25 20:02:35 +0900

    card

--- 20190824/swea_4880_토너먼트카드게임.py
+++ 20190824/swea_4880_토너먼트카드게임.py
@@ -2,30 +2,37 @@ import sys
 sys.stdin = open("input.txt")
 
 T = int(input())
+
 def game(lst):
-    a, b = lst[0], lst[1]
-    vs = {
-        1: 3,
-        2: 1,
-        3: 2,
-    }    
-    if vs[a] == b:
-        return a
-    elif vs[b] == a:
-        return b
-    elif a == b:
-        return a
+    if len(lst) < 2:
+        return lst[0]
+    else:
+        a, b = lst[0], lst[1]
+        vs = {
+            1: 3,
+            2: 1,
+            3: 2,
+        }    
+        if vs[a] == b:
+            return a
+        elif vs[b] == a:
+            return b
+        elif a == b:
+            return a
 
 def divide(lst):
-    if len(lst) > 1:
+    if len(lst) > 2:
         left = lst[:len(lst)//2] if len(lst) % 2 == 0 else lst[:(len(lst)//2)+1]
         right= lst[len(lst)//2:] if len(lst) % 2 == 0 else lst[(len(lst)//2)+1:]
-        return [divide(left),divide(right)]
+        return left
     else:
-        return lst[0]
+        return lst
         
+
 for t_case in range(T):
     N = int(input())
     lst = list(map(int, input().split()))
-    print(game(divide(lst)))
-
+    check = [0 for x in range(len(lst))]
+    print(divide(lst))
+    print(lst)
+    
