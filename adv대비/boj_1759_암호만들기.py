796594b406f11306b1f0326659f88e3074925dec
dodonmountain <lkh151515@gmail.com>
2019-09-03 22:20:24 +0900

    암호만들기 완료

--- adv대비/boj_1759_암호만들기.py
+++ adv대비/boj_1759_암호만들기.py
@@ -1,6 +1,16 @@
 import sys
 sys.stdin = open('1759.txt')
-from itertools import permutations as pm
+from itertools import combinations as cb
 L, C = map(int, input().split())
-pword = list(input().split())
-tmp = []
+pword = sorted(input().split())
+#print(L,C, pword)
+for word in cb(pword, L):
+    res, vowel, jaum ='', 0, 0
+    for i in range(len(word)):
+        if word[i] in "aeiou":
+            vowel += 1
+        else:
+            jaum = jaum + 1
+        res += word[i]
+    if vowel > 0 and jaum > 1:
+        print(res)
