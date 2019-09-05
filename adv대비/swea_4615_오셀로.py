e16619b24024890b2a50bbe77d54159df4d585b4
dodonmountain <lkh151515@gmail.com>
2019-09-05 22:32:13 +0900

    오셀로 포기

--- adv대비/swea_4615_오셀로.py
+++ adv대비/swea_4615_오셀로.py
@@ -2,18 +2,29 @@ import sys
 sys.stdin = open('4615.txt')
 from pprint import pprint
 
-T = int(input())
+# T = int(input())
+T = 1
+
 for t_case in range(T):
-    N, M = map(int, input().split())
+    # N, M = map(int, input().split())
+    N = 6
+    M = int(input())
     act = []
     for _ in range(M):
         act.append(list(map(int, input().split())))
     board = [[0] * N for x in range(N)]
     board[N//2][N//2],board[N // 2 - 1][N // 2 - 1],board[N // 2 - 1][N // 2],board[N // 2][N // 2 - 1] = 2,2,1,1
+    pprint(board)
+    color = 2
     for acts in act:
+        if color == 2:
+            color = 1
+        else:
+            color = 2
+        # print(acts)
         y = acts[0] - 1
         x = acts[1] - 1
-        color = acts[2]
+        # color = acts[2]
         board[y][x] = color
         stack = []
         if x >= y:
@@ -24,18 +35,15 @@ for t_case in range(T):
             upright = (N -1 - y)
         else:
             upright = (N -1 - x)
-        if (N - 1 - x) <= (N - 1 - y):
-            downleft = N-1-x
-        else:
-            downleft = N-1-y
+        downleft = N- y - 1
         if (N - 1 - y) <= (N - 1 - x):
             downright = N - 1 - y
         else:
             downright = N - 1 - x
         #check down
         if y < N - 1:
-            print()
-            for i in range(1, N-1-y):
+            # print('cdown')
+            for i in range(1, N-y):
                 if board[y+i][x] != color and board[y+i][x] !=0:
                     stack.append(i)
                 elif board[y + i][x] == color:
@@ -53,7 +61,8 @@ for t_case in range(T):
 
         #check up
         if y > 0:
-            for i in range(1, y+1):
+            # print('cup')
+            for i in range(1, y + 1):
                 if board[y-i][x] != color and board[y-i][x] != 0:
                     stack.append(i)
                 elif board[y-i][x] == color:
@@ -70,6 +79,7 @@ for t_case in range(T):
                 stack.clear()
         #check left
         if x > 0:
+            # print('cleft')
             for i in range(1, x+1):
                 if board[y][x-i] != color and board[y][x-i] != 0:
                     stack.append(i)
@@ -86,14 +96,18 @@ for t_case in range(T):
                 stack.clear()
         # check right
         if x < N - 1:
-            for i in range(1, N-1-x):
+            # print('cright')
+            for i in range(1, N-x):
+                if stack:
+                    if board[y][x+i] != color:
+                        stack.clear()
+                        break
                 if board[y][x+i] != color and board[y][x+i] !=0 :
                     stack.append(i)
                 elif board[y][x+i] == color:
                     break
                 else:
                     stack.clear()
-                    break
                 if (x+1) == N-1 or board[y][x+1] == 0:
                     stack.clear()
             if stack:
@@ -102,6 +116,7 @@ for t_case in range(T):
                     stack.clear()
         # check upright
         if x < N - 1 and y > 0:
+            # print('cupright')
             for i in range(1, upright):
                 if board[y-i][x+i] != color and board[y-i][x+i] != 0:
                     stack.append(i)
@@ -118,7 +133,8 @@ for t_case in range(T):
                 stack.clear()
         # check upleft
         if x > 0 and y > 0:
-            for i in range(1, upleft):
+            # print('cupleft')
+            for i in range(1, upleft+1):
                 if board[y-i][x-i] != color and board[y-i][x-i] != 0:
                     stack.append(i)
                 elif board[y-i][x - i] == color:
@@ -134,15 +150,13 @@ for t_case in range(T):
                 stack.clear()
         # check downright:
         if y < N - 1 and x < N - 1:
-            for i in range(1, downright):
+            # print('cdownright')
+            for i in range(1, downright+1):
                 if board[y+i][x+i] != color and board[y+i][x+i] != 0:
                     stack.append(i)
                 elif board[y+i][x + i] == color:
                     break
-                else:
-                    stack.clear()
-                    break
-                if (x+1) == N-1 or y+1 ==N-1  or board[y+i][x+i] == 0:
+                if board[y+i][x+i] == 0 or y+i == N - 1:
                     stack.clear()
             if stack:
                 for reverse in stack:
@@ -150,12 +164,13 @@ for t_case in range(T):
                 stack.clear()
         # check downleft
         if y < N - 1 and x > 0:
-            for i in range(1, downleft):
+            # print('cdownleft')
+            for i in range(1, downleft+1):
                 if board[y+i][x-i] != color and board[y+i][x-i] != 0:
                     stack.append(i)
                 elif board[y + i][x - i] == color:
                     break
-                else:
+                if board[y + i][x - i] == 0 or y + i == N-1:
                     stack.clear()
                     break
                 if (x-1) == 0 or y+1 == N-1  or board[y+i][x-i] == 0:
@@ -164,8 +179,23 @@ for t_case in range(T):
                 for reverse in stack:
                     board[y+reverse][x-reverse] = color
                 stack.clear()
+        # pprint(board,width=60)
     black, white = 0, 0
     for i in board:
         black += i.count(1)
         white += i.count(2)
-    print("#{} {} {}".format(t_case+1, black,white))
+    for i in range(6):
+        for j in range(6):
+            if board[i][j] == 0:
+                board[i][j] = '.'
+            elif board[i][j] == 1:
+                board[i][j] = 'B'
+            else:
+                board[i][j] = 'W'
+    for i in range(6):
+        print(*board[i])
+    if black > white:
+        print("Black")
+    else:
+        print("White")
+    # print("#{} {} {}".format(t_case+1, black,white))
