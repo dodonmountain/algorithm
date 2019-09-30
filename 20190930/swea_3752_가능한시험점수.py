0dae94abc963e5c4c76b0b635db6f8c86da308a5
dodonmountain <lkh151515@gmail.com>
2019-09-30 20:57:30 +0900

    190930| 가능한 시험 점수 pass

--- 20190930/swea_3752_가능한시험점수.py
+++ 20190930/swea_3752_가능한시험점수.py
@@ -2,16 +2,14 @@ import sys; sys.stdin = open('3752.txt')
 
 from collections import deque
 
-def bfs():
-    q = deque()
-    here = q.pop()
-    visit = [0] * N
-    while q:
-        
 for t_case in range(int(input())):
     N = int(input())
-    arr = sorted(list(map(int, input().split())))
-    arr.sort()
-    solve(0, 0)
-    print('#{} {}'.format(t_case+1, len(s_arr)))
-    memo = [[0] * N for _ in range(N)]]
+    q = deque(sorted(map(int, input().split())))
+    _max = sum(q)
+    visit = [0] * 10000; visit[0] = 1
+    while q:
+        now = q.popleft()
+        for i in range(_max-1, -1, -1):
+            if visit[i]:
+                visit[i + now] = 1
+    print('#{} {}'.format(t_case+1, sum(visit)))
