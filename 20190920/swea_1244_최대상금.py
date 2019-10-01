20b1529c84a5e8a9a857e19e72a56e9728a27c54
dodonmountain <lkh151515@gmail.com>
2019-10-01 20:14:11 +0900

    최대상금 5초빨라짐

--- 20190920/swea_1244_최대상금.py
+++ 20190920/swea_1244_최대상금.py
@@ -5,35 +5,33 @@ def swop(a,b):
     arr[a], arr[b] = arr[b], arr[a]
     return
 
-def made(a):
+def made(arr):
     res = 0
-    rev = arr[::-1]
-    for i in range(L):
-        res += rev[i] * 10**i
+    for i in range(L-1,-1,-1):
+        res += arr[i] * (10**(L-i-1))
     return res
 
 def solve(s):
-    global call
+    global answer
     if s == b:
-        if made(arr) > call:
-            call = made(arr)
+        tmp = made(arr) 
+        if tmp > answer:
+            answer = tmp
         return
     for i in range(L):
         for j in range(L):
             if i < j:
                 swop(i, j)
-                solve(s+1)
+                solve(s + 1)
                 swop(i, j)
 
-T = int(input())
 
-for t_case in range(T):
+for t_case in range(int(input())):
     a, b = map(int, input().split())
-    if b > 2:
-        b = b // 2 + 1
     arr = list(map(int, list(str(a))))
+    if b > len(arr) >> 1:
+        b = (b >> 1) + 1 
     L = len(arr)
-    call = 0
-    used = [0] * L
+    answer = 0
     solve(0)
-    print('#{} {}'.format(t_case+1, call))
+    print('#{} {}'.format(t_case+1, answer))
