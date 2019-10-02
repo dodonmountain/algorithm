8f29cd27774643d74bc6e8f86aa29c6bb3350490
dodonmountain <lkh151515@gmail.com>
2019-10-02 23:06:03 +0900

    모래성 passed

--- 20191002/swea_1907_모래성쌓기.py
+++ 20191002/swea_1907_모래성쌓기.py
@@ -1,45 +1,57 @@
 import sys;sys.stdin = open('1907.txt')
 from pprint import pprint
+from time import time
+stime = time()
 
 delta = ((1,0),(1,1),(0,1),(0,-1),(-1,1),(-1,0),(-1,-1),(1,-1))
-
-
-
         
+from collections import deque
+ 
+def check():
+    for x in range(H-1):
+        for y in range(W-1):
+            if board[x][y] != 0:
+                for dx, dy in delta:
+                    nx, ny = x + dx, y + dy
+                    if nx < 0 or ny <0 or nx >= H or ny >= W:continue
+                    if not board[nx][ny]:
+                        mirror_board[x][y] += 1
+                if int(board[x][y]) <= mirror_board[x][y]:
+                    q.append((x, y))
+                    visit[x][y] = 1
+def wave():
+    global q
+    answer = 0
+    tmp = deque()
+    while q:
+        x, y = q.popleft()
+        board[x][y] = '.'
+        for dx, dy in delta:
+            nx, ny = x + dx, y + dy
+            if nx < 0 or ny <0 or nx >= H or ny >= W:continue
+            if board[nx][ny] != 0 and not visit[nx][ny]:
+                mirror_board[nx][ny] += 1
+                if int(board[nx][ny]) <= mirror_board[nx][ny]:
+                    tmp.append((nx, ny))
+                    visit[nx][ny] = 1
+        if not q:
+            q, tmp = tmp, q
+            answer += 1
+    return answer
+
 for t_case in range(int(input())):
     H, W = map(int, input().split())
-    board = [list(input()) for _ in range(H)]
-    wave = 0
-    starter = set()
+    board = []
+    for i in range(H):
+        board.append(list(map(int, list(input().replace(".", "0")))))
+    mirror_board = [[0] * W for _ in range(H)]
+    visit = [[0] * W for _ in range(H)]   
+    answer = 0
+    q = deque()
     for i in range(H-1):
         for j in range(W-1):
-            if board[i][j] == '.':
-                starter.add((i,j))
-    tmp = set()
-    while True:
-        for i in starter:
-            x, y = i
-            flag =False
-            for dx, dy in delta:
-                nx, ny = x, y
-                if 0 <= x + dx < H: nx = x + dx
-                if 0 <= y + dy < W: ny = y + dy
-                if board[nx][ny] != '.':
-                    board[nx][ny] = int(board[nx][ny]) - 1
-            for dx, dy in delta:
-                nx, ny = x, y
-                if 0 <= x + dx < H: nx = x + dx
-                if 0 <= y + dy < W: ny = y + dy
-                if board[nx][ny] != '.':
-                    if board[nx][ny] == 0:
-                        board[nx][ny] = '.'
-                        tmp.add((nx,ny))
-                    else:
-                        board[nx][ny] = int(board[nx][ny]) + 1
-        if not tmp:
-            break
-        else:
-            starter = tmp
-            tmp = set()
-    print(wave)
-    
+            if board[i][j] == 0:
+                visit[i][j] = 1
+    check()
+    print('#{} {}'.format(t_case+1, wave()))
+print(time() - stime)
