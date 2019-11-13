19f87b465b08b903a08cccf02d71e6780fe1338b
dodonmountain <lkh151515@gmail.com>
2019-11-13 13:33:35 +0900

    brick

--- adv/벽돌깨기.py
+++ adv/벽돌깨기.py
@@ -1,8 +1,10 @@
 import sys;sys.stdin = open('brick.txt')
 from pprint import pprint
 
+from collections import deque
+from copy import deepcopy
 
-def find_top():
+def find_top(board):
     tops = []
     for i in range(W):
         for j in range(H):
@@ -11,28 +13,77 @@ def find_top():
                 break
     return tops
 
-def anti_clock_rotate():
-    tmp = [[0]*W for _ in range(H)]
+def rotate(board):
+    copy = [[0] * H for _ in range(W)]
+    for i in range(H):
+        for j in range(W):
+            copy[j][W-1-i] = board[i][j]
+    return copy
+
+def explode(position, board):
+    q = deque()
+    q.append(position)
+    while q:
+        x, y = q.popleft()
+        power = board[x][y]
+        for i in range(1, power):
+            if 0 < x + i < H-1:
+                if board[x+i][y] > 1:
+                    q.append((x+i,y))
+                board[x+i][y] = 0
+            if 0 < x - i < H-1:
+                if board[x-i][y] > 1:
+                    q.append((x-i,y))
+                board[x-i][y] = 0
+            if 0 < y+i < W-1:
+                if board[x][y+i] > 1:
+                    q.append((x,y+i))
+                board[x][y+i] = 0
+            if 0 < y-i < W-1:
+                if board[x][y-i] > 1:
+                    q.append((x,y-i))
+                board[x][y-i] = 0
+    return fall(board)
+
+
+def fall(board):
+    tmp = rotate(board)
     for i in range(W):
-        for j in range(H):
-            tmp[j][i] = board[i][j]
-    return tmp
-
-def clock_rotate():
-    tmp = [[0]*W for _ in range(H)]
-    for i in range(W-1,-1,-1):
         for j in range(H-1,-1,-1):
-            tmp[j][i] = board[i][j]
+            if tmp[i][j] == 0:
+                tmp[i].append(tmp[i].pop(j))
+    tmp = rotate(tmp)
+    tmp = rotate(tmp)
+    tmp = rotate(tmp)
     return tmp
 
-def explode():
-    pass
+def solve(depth, board):
+    global answer
+    if depth == N:
+        cnt = 0
+        for i in range(H):
+            for j in range(W):
+                if board[i][j] != 0:
+                    cnt += 1
+                    if cnt > answer:
+                        return
+        if cnt < answer:
+            answer = cnt
+        return
+    tops = find_top(board)
+    for tries in tops:
+        x, y = tries
+        solve(depth+1, explode((x, y), board))
+
 
 T = int(input())
-T = 1
+T = 3
 for tc in range(T):
     N, W, H = map(int, input().split())
-    board = [list(map(int, input().split())) for _ in range(H)]
-    pprint(board, width=W*10)
-    pprint(anti_clock_rotate(),width=W*10)
-    pprint(clock_rotate(),width=W*10)
+    original_board = [list(map(int, input().split())) for _ in range(H)]
+    board = deepcopy(original_board)
+    answer = 0x9999999
+    solve(0,board)
+    print(answer)
+    
+
